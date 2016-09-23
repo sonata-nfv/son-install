816f6514a5c1db8c533029e844464794cbc9528a
Alberto Rocha <arocha@ptinovacao.pt>
2016-09-23 16:18:32 +0000

    v1.0 - SONATA NFV Service Platform deployment - templates for Inventory

--- terraform/v00/qual/bootstrap-son-u14.sh
+++ terraform/v00/qual/bootstrap-son-u14.sh
@@ -6,4 +6,7 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible -y
 sudo apt-get install git -y
+git clone https://github.com/sonata-nfv/son-install.git
+cd son-install
+ansible-playbook son-cmud.yml -e "target=localhost operation=install service=all"
 
