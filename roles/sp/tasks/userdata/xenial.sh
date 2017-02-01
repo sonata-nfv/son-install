cd4a4c272328003aa2a198df9420e6ec925cb5f9
Alberto Rocha <arocha@ptinovacao.pt>
2017-02-01 16:13:38 +0000

    SONATA CMUTD v2.0 - Create, Manage, Upgrade, Test and Destroy SONATA environments

---
+++ roles/sp/tasks/userdata/xenial.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible -y
+sudo apt-get install git -y
+sudo apt-get install htop nmap tree sysstat -y
+
