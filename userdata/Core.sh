7aaafd7042e51559fc5797879fba8682e911e2d2
Alberto Rocha <arocha@ptinovacao.pt>
2017-02-21 10:24:28 +0000

    SONATA CMUD v2.0 - Create, Manage, Upgrade and Destroy SONATA environments

--- userdata/Core.sh
+++ userdata/Core.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
-sudo yum update 
-sudo yum install ansible -y
-sudo yum install git -y
-sudo yum install python-pip python-setuptools python-wheel -y
-sudo yum install htop nmap tree sysstat -y
+yum update -y
+yum upgrade -y
+yum install python-pip python-setuptools python-wheel -y
+yum install git nmap tree sysstat -y
+yum install epel-release -y
+yum update -y
+yum install ansible htop -y
