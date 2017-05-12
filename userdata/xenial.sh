04a5ff6cac1568a224639d49a559854e2c167ea2
Alberto Rocha <arocha@ptinovacao.pt>
2017-05-12 20:15:52 +0000

    SONATA 5G NFV CMUD v2.1 (Create, Manage, Upgrade and Destroy) - versioning

--- userdata/xenial.sh
+++ userdata/xenial.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-apt-get update -y
+apt-get update
 apt-get install python -y
-apt-get install software-properties-common -y
-apt-add-repository ppa:ansible/ansible
-apt-get update -y
-apt-get install ansible -y
-apt-get install git -y
-apt-get install htop nmap tree sysstat -y
+apt-get install git htop nmap tree sysstat -y
 
